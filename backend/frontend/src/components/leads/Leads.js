6cbd54603c3e6e6b896c200b8b5a9ddd9649831f
NGONIDZASHE NZENZE <ngonidzashedelight@gmail.com>
2023-01-30 11:51:10 +0200

    Added react changes

--- backend/frontend/src/components/leads/Leads.js
+++ backend/frontend/src/components/leads/Leads.js
@@ -1,6 +1,14 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { getLeads } from '../../actions/leads';
+
 
 export class Leads extends Component {
+  static PropTypes = {
+    leads: PropTypes.array.isRequired
+  };
+
   render() {
     return (
       <div>
@@ -10,4 +18,8 @@ export class Leads extends Component {
   }
 }
 
-export default Leads
+const mapStateToProps = state => ({
+  leads: state.leads.leads
+});
+
+export default connect(mapStateToProps)(Leads);
